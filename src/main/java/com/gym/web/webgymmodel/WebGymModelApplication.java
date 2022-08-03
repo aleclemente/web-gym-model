56500de003e30920ba7c97643161b992565678d7
Alexandre Clemente <aleclemente@gmail.com>
2022-08-02 21:25:13 -0300

    First commit with springboot

---
+++ src/main/java/com/gym/web/webgymmodel/WebGymModelApplication.java
@@ -0,0 +1,13 @@
+package com.gym.web.webgymmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGymModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebGymModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gym/web/webgymmodel/WebGymModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.web.webgymmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGymModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
